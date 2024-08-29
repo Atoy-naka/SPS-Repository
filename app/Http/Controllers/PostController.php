3921314b4d1b995b63b3a18417aaadeb01100060
Atoy-naka <nakamura_1009@outlook.jp>
2024-08-29 16:40:38 +0900

    投稿作成処理実行時のバリデーション実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -11,18 +11,18 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    
+
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
     }
-    
+
     public function create()
     {
         return view('posts.create');
     }
-    
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,27 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>投稿</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <title>Blog</title>
     </head>
     <body>
-        <h1>Post</h1>
+        <h1>Blog Name</h1>
         <form action="/posts" method="POST">
             @csrf
-            <div class="title"></div>
+            <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトルを入力"/>
+                <input type="text" name="post[title]" placeholder="タイトルを入力" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>本文</h2>
-                <textarea name="post[body]" placeholder="本文を入力"></textarea>
+                <textarea name="post[body]" placeholder="本文を入力">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="投稿"/>
+            <input type="submit" value="保存"/>
         </form>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+        <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
