3a7bdb2656f36d1edddde02a193d41cbbfd2dc8b
Atoy-naka <nakamura_1009@outlook.jp>
2024-09-07 17:56:18 +0900

    プロフィールアイコン画像機能の追加

--- database/migrations/2024_08_28_170146_create_posts_table.php
+++ database/migrations/2024_08_28_170146_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained();
+            $table->foreignId('category_id')->nullable()->constrained();
             $table->string('title', 30);
             $table->string('body', 3000);
             $table->timestamps();
