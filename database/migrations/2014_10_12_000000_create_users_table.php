65ce84d2595cae6290b6ea90d8eff65d49fc3fcf
Atoy-naka <nakamura_1009@outlook.jp>
2024-09-21 18:03:12 +0900

    アイコン画像更新

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('profile_photo_path')->nullable(); //プロフィールアイコンのパスを保存するカラム
+            $table->text('profile_photo_path')->nullable(); //プロフィールアイコンのパスを保存するカラム
             $table->rememberToken();
             $table->timestamps();
         });
